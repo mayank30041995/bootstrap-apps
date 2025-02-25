dc03b12400308a0857eaf728103822d35e919cb2
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-02-25 16:48:56 +0530

    added all projects

---
+++ 6-setup/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // ripples
+    $("#header, .info").ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.04,
+        imageUrl: null
+    });
+
+
+    
+
+
+});
